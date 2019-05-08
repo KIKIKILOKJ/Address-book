2e495d6df334a899756e23958c86e16cf70304ac
peter max <petermax2004@gmail.com>
2019-05-08 14:25:50 +0300

    Add the features

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function contact(first, last){
   this.lastName = last;
 }
 
+contact.prototype.fullName = function(){
+  return this.firstName + "" + this.lastName;
+}
+
 
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
@@ -13,9 +17,16 @@ $(document).ready(function(){
 
     var newContact = new contact(inputtedFirstName, inputtedLatName);
 
-    $("ul#contacts").append("<li><span class=`contact`>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class=`contact`>" + newContact.fullName + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
   })
 })
+
+$(".contact").last().click(function(){
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.firstName);
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+});
